61d381891dc85915033ca2c468a38733868b2a65
pfen0005 <pfen0005@student.monash.edu>
2024-09-10 16:48:06 +1000

    added Assignment 2 starting point from A1

--- src/game/Application.java
+++ src/game/Application.java
@@ -7,6 +7,18 @@ import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.FancyGroundFactory;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.World;
+import game.actors.FurnaceGolem;
+import game.actors.Player;
+import game.displays.FancyMessage;
+import game.grounds.Dirt;
+import game.grounds.Floor;
+import game.grounds.Puddle;
+import game.grounds.Wall;
+import game.consumables.FlaskOfHealing;
+import game.consumables.FlaskOfRejuvenation;
+import game.consumables.ShadowTreeFragment;
+import game.weapons.GreatKnife;
+import game.weapons.ShortSword;
 
 /**
  * The main class to start the game.
@@ -52,8 +64,38 @@ public class Application {
         Player player = new Player("Tarnished", '@', 150);
         world.addPlayer(player, gameMap.at(7, 4));
 
+        // Place the ShortSword at coordinates (x=7, y=8)
+        ShortSword shortSword = new ShortSword();
+        gameMap.at(7, 8).addItem(shortSword);
+
+        // Place the ShortSword at coordinates (x=5, y=8)
+        GreatKnife greatKnife = new GreatKnife();
+        gameMap.at(5, 8).addItem(greatKnife);
+
+        FlaskOfHealing flaskOfHealing = new FlaskOfHealing();
+        gameMap.at(10, 9).addItem(flaskOfHealing);
+
+        FlaskOfRejuvenation flaskOfRejuvenation = new FlaskOfRejuvenation();
+        gameMap.at(14, 9).addItem(flaskOfRejuvenation);
+
+        ShadowTreeFragment shadowTreeFragment1 = new ShadowTreeFragment();
+        gameMap.at(20, 8).addItem(shadowTreeFragment1);
+
+        ShadowTreeFragment shadowTreeFragment2 = new ShadowTreeFragment();
+        gameMap.at(18, 4).addItem(shadowTreeFragment2);
+
+        ShadowTreeFragment shadowTreeFragment3 = new ShadowTreeFragment();
+        gameMap.at(16, 2).addItem(shadowTreeFragment3);
+
+        ShadowTreeFragment shadowTreeFragment4 = new ShadowTreeFragment();
+        gameMap.at(14, 5).addItem(shadowTreeFragment4);
+
+        ShadowTreeFragment shadowTreeFragment5 = new ShadowTreeFragment();
+        gameMap.at(22, 3).addItem(shadowTreeFragment5);
+
         gameMap.at(42, 4).addActor(new FurnaceGolem());
 
         world.run();
+
     }
 }
