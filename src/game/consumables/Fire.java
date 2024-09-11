65e8e6c4dcfb8984f98897377f4f143d5d24eed4
briclare <bvau0003@student.monash.edu>
2024-09-11 12:43:47 +1000

    Updated Fire class to add BurningStatusEffect

--- src/game/consumables/Fire.java
+++ src/game/consumables/Fire.java
@@ -1,33 +1,45 @@
 package game.consumables;
 
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.actors.StatusEffect;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.items.Item;
 import edu.monash.fit2099.engine.positions.Location;
+import game.actors.BurningStatusEffect;
 import game.enums.Ability;
 import game.enums.Status;
 
+/**
+ * Class represents a fire item that can cause burning damage to actors.
+ * The fire persists for a certain number of turns and inflicts damage to actors who stand on it.
+ */
 public class Fire extends Item {
-    private int FIRE_DAMAGE = 5;
     private int MAX_BURN_DURATION = 5;
+    private int burnDuration = 0;
 
     public Fire() {
         super("Fire", 'w', false);
     }
 
+    /**
+     * Manages the duration of the fire.
+     * Increases burn duration and removes the fire from the location if it has reached the MAX_BURN_DURATION
+     * Applied burning damage present to actors in the location that are not fire-resistant
+     *
+     * @param location The location of the ground on which we lie.
+     */
     @Override
     public void tick(Location location) {
         super.tick(location);
-        MAX_BURN_DURATION--;
-        if (MAX_BURN_DURATION == 0) {
-            location.removeItem(this);// Remove the fire once the burn duration has passed.
+        burnDuration++;
+        if (burnDuration == MAX_BURN_DURATION) {
+            location.removeItem(this); // Remove the fire once the burn duration has passed.
         }
 
         if (location.containsAnActor()) {
             Actor actor = location.getActor();
                 if (!actor.hasCapability(Ability.FIRE_RESISTANT)) {
-                    actor.hurt(FIRE_DAMAGE);
-                    new Display().println(String.format("\nThe explosion burns %s for %d damage!", actor, FIRE_DAMAGE)); // Add BURNING status to the actor
+                    actor.addStatusEffect(new BurningStatusEffect());
             }
         }
     }
