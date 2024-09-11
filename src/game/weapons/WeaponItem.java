25264deb6e232c47303c108ff225ddb8f58bf94b
briclare <bvau0003@student.monash.edu>
2024-09-11 13:26:08 +1000

    Ammendment of allowableactions method to check if otherActor is enemy

--- src/game/weapons/WeaponItem.java
+++ src/game/weapons/WeaponItem.java
@@ -9,12 +9,16 @@ import edu.monash.fit2099.engine.positions.Location;
 import edu.monash.fit2099.engine.weapons.Weapon;
 import game.enums.NewActorAttributes;
 import game.actions.AttackAction;
+import game.enums.Status;
 
 
 import java.util.Random;
 
 /**
- * Class representing items that can be used as a weapon.
+ * An abstract class representing a weapon item
+ * Extends the Item class and implements the Weapon interface providing basic function
+ * for weapons such as attacking and picking up the weapon, as well as handling
+ * weapon damage, hit rate and strength requirements
  */
 public abstract class WeaponItem extends Item implements Weapon {
     private static final float DEFAULT_DAMAGE_MULTIPLIER = 1.0f;
@@ -45,11 +49,12 @@ public abstract class WeaponItem extends Item implements Weapon {
     }
 
     /**
+     * Executes the attack with the weapon
      *
      * @param attacker the actor who performed the attack
      * @param target   the actor who is the target of the attack
      * @param map      the map on which the attack was executed
-     * @return
+     * @return A string describing the result of the attack
      */
     @Override
     public String attack(Actor attacker, Actor target, GameMap map) {
@@ -64,27 +69,41 @@ public abstract class WeaponItem extends Item implements Weapon {
     }
 
     /**
+     * Returns a pick-up actions if the actor has sufficient strength to wield a weapon
      *
-     * @param actor
-     * @return
+     * @param actor The actor attempting to pick up the weapon
+     * @return A PickUpAction if the actor has enough strength, otherwise null
      */
     @Override
     public PickUpAction getPickUpAction(Actor actor) {
-        if (strengthReq <= actor.getAttributeMaximum(NewActorAttributes.STRENGTH)) {
-            return new PickUpAction(this);
+        if (actor.hasCapability(NewActorAttributes.STRENGTH)) {
+            if (strengthReq <= actor.getAttributeMaximum(NewActorAttributes.STRENGTH)) {
+                return new PickUpAction(this);
+            }
         }
         return null;
     }
 
+    /**
+     * List of allowable actions an actor can perform with a WeaponItem
+     * i.e. if there is an enemy in the surroundings, a player can use a WeaponItem to attack it
+     *
+     * @param otherActor the actor the action is against
+     * @param location the location of the other actor
+     * @return an unmodifiable list of Actions
+     */
 
     @Override
-    public ActionList allowableActions(Actor actor, Location location) {
-        ActionList actions = super.allowableActions(location);
-        actions.add(new AttackAction(actor, location.toString(), this));
-        actions.add(getDropAction(actor));
+    public ActionList allowableActions(Actor otherActor, Location location) {
+        ActionList actions = new ActionList();
+        if (otherActor.hasCapability(Status.ENEMY)) {
+            actions.add(new AttackAction(otherActor, location.toString(), this));
+        }
         return actions;
     }
 }
+
+
 
 
 
