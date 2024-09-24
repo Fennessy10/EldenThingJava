7b418b90dcef49c2c32bf55b133fa47d0f07c639
pfen0005 <pfen0005@student.monash.edu>
2024-09-24 12:10:59 +1000

    subclasses don't need to implement the same interface as the superclass (e.g Each weapon class should not implement the Weapon interface because WeaponItem has implemented the Weaponinterface)

--- src/game/weapons/GreatKnife.java
+++ src/game/weapons/GreatKnife.java
@@ -4,9 +4,7 @@ package game.weapons;
  * Class representing The Short Sword Weapon
  */
 
-import edu.monash.fit2099.engine.weapons.Weapon;
-
-public class GreatKnife extends WeaponItem implements Weapon{
+public class GreatKnife extends WeaponItem{
     public GreatKnife(){
         super("GreatKnife",'†', 75, "attack", 60, 5);
     }
--- src/game/weapons/ShortSword.java
+++ src/game/weapons/ShortSword.java
@@ -4,9 +4,7 @@ package game.weapons;
  * Class representing The Short Sword Weapon
  */
 
-import edu.monash.fit2099.engine.weapons.Weapon;
-
-public class ShortSword extends WeaponItem implements Weapon{
+public class ShortSword extends WeaponItem{
     public ShortSword(){
         super("ShortSword",'!', 100, "attack", 75, 10);
     }
