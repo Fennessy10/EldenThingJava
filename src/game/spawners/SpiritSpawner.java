38865cd4b0b8756e0bfca8dd6f89978db5c27a38
Yuchen0820 <ylii0566@student.monash.edu>
2024-09-30 01:00:10 +1000

    REQ3_commit_6: Add commits about SpiritSpawner, including the SpiritSpawner class, constructor, and concrete implementation of abstract functions to create a new spirit.

--- src/game/spawners/SpiritSpawner.java
+++ src/game/spawners/SpiritSpawner.java
@@ -3,12 +3,24 @@ package game.spawners;
 import edu.monash.fit2099.engine.actors.Actor;
 import game.actors.Spirit;
 
+/**
+ * The SpiritSpawner class is responsible for spawning {@code Spirit} actors
+ * in the game. It extends the {@link Spawner} class and provides a specific
+ * implementation of the {@link #spawn()} method, which creates a new {@link Spirit} actor.
+ */
 public class SpiritSpawner extends Spawner{
 
+    /**
+     * Constructs a new SpiritSpawner with a fixed spawn chance of 20%
+     */
     public SpiritSpawner(){
         super(0.2);
     }
 
+    /**
+     * spawn a new actor: Spirit
+     * @return a newly created instance
+     */
     @Override
     public Actor spawn(){
         return new Spirit();
