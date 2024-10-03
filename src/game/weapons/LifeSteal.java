59b44f4f9d74a3da605e46c16544becc66732704
lastlyss <sais0004@student.monash.edu>
2024-10-03 21:38:13 +1000

    Add: Javadoc to LifeSteal, Quickstep and WeaponArt classes

--- src/game/weapons/LifeSteal.java
+++ src/game/weapons/LifeSteal.java
@@ -2,16 +2,37 @@ package game.weapons;
 
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
-
+/**
+ * Class representing the life steal weapon art
+ * Extends the WeaponArt class and allows the player to move to regenerate health after attacking another actor
+ * when using a weapon with the life steal weapon art.
+ * Mana cost to use the life steal weapon art is 10.
+ * Heals 20 points when used
+ *
+ *  * Created by:
+ *  * @author Sebastian Aisea
+ */
 public class LifeSteal extends WeaponArt{
 
     private final static int MANA_COST = 10;
     private final static int HEALING_POINTS = 20;
 
+    /**
+     * Constructor to create a new Life Steal weapon art.
+     */
     public LifeSteal() {
         super("Life Steal", MANA_COST);
     }
 
+
+    /**
+     * Executes the Life Steal effect, healing the player if they have enough mana.
+     * If the player does not have sufficient mana, the life steal effect does not activate.
+     *
+     * @param player The player using this weapon art.
+     * @param map The game map where the action is taking place.
+     * @return A string indicating the result of using Life Steal.
+     */
     @Override
     public String execute(Actor player, GameMap map){
         player.heal(HEALING_POINTS);
--- src/game/weapons/WeaponArt.java
+++ src/game/weapons/WeaponArt.java
@@ -2,10 +2,24 @@ package game.weapons;
 
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
+/**
+ * An abstract class representing a weapon art
+ *
+ * * Created by:
+ *  * @author Sebastian Aisea
+ *
+ **/
 
 public abstract class WeaponArt {
     protected String name;
     protected int manaCost;
+
+    /**
+     * Constructor to initialize a WeaponArt with its name and mana cost.
+     *
+     * @param name the name of the weapon art
+     * @param manaCost the mana cost of using the weapon art
+     */
 
     public WeaponArt(String name, int manaCost){
         this.name = name;
