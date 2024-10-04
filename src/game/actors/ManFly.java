eda61ff39618d8d6b92e09206e2689acc433f3c8
pfen0005 <pfen0005@student.monash.edu>
2024-10-04 15:02:21 +1000

    ManFly attacks now work

--- src/game/actors/ManFly.java
+++ src/game/actors/ManFly.java
@@ -14,6 +14,8 @@ import edu.monash.fit2099.engine.positions.Location;
 import game.enums.Ability;
 import game.enums.ManFlyPoisonEffect;
 import game.enums.Status;
+import game.weapons.ManFlyStinger;
+import game.weapons.StompingFoot;
 
 import java.util.AbstractMap;
 import java.util.Map;
@@ -23,20 +25,20 @@ import java.util.Random;
  * New actor class for man_fly which wandering in the sewers
  * man_fly can attack nearby actors by poison and following it
  */
-public class ManFly extends Actor {
-    private final Random random = new Random();
-    private WanderBehaviour wanderBehaviour;
-    private FollowBehaviour followBehaviour;
+public class ManFly extends Enemy {
+    private static final int manFlyHP = 50;
+    private static final char displayChar = '%';
+    private static final String name = "ManFly";
 
     /**
      * constructor for man_fly
      * initialize it behaviour, char and hit point(50)
      */
     public ManFly() {
-        super("ManFly",'%',50);
-        this.wanderBehaviour = new WanderBehaviour(); //use wanderbehaviour we creat before
+        super(name, displayChar, manFlyHP);
         this.addCapability(Status.ENEMY);
         this.addCapability(Ability.POISON_RESISTANT);
+        this.setIntrinsicWeapon(new ManFlyStinger());
     }
 
 
@@ -50,78 +52,15 @@ public class ManFly extends Actor {
      */
     @Override
     public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
-        ActionList actions = new ActionList();
-        if(otherActor.hasCapability(Status.HOSTILE_TO_ENEMY)){
-            actions.add(new AttackAction(this,direction));
+        ActionList actions = new ActionList(); //create a list of actions
+
+        if (otherActor.hasCapability(Status.FOLLOW)) {
+            super.behavioursPut(500, new FollowBehaviour(otherActor));
+        }
+        if (otherActor.hasCapability(Status.HOSTILE_TO_ENEMY)) {
+            actions.add(new AttackAction(this, direction));
         }
         return actions;
     }
 
-
-    /**
-     * Determine action for man_fly in each turn
-     * set wandering as default state
-     * when actor near it, try to attack by poison
-     * keep follow the Tarnished around until either of them dies
-     *
-     * @param actions actions for man_fly
-     * @param lastAction lastAction perform
-     * @param map map for the game
-     * @param display show output
-     * @return man_fly's action in turns
-     */
-    @Override
-    public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
-        for (Exit exit : map.locationOf(this).getExits()) {
-            //caheck if here exit a actor
-            if (exit.getDestination().containsAnActor()) {
-                Actor nearbyActor = exit.getDestination().getActor(); // get the first actor which the exit
-                //check if this actor is hostile
-                if (nearbyActor.hasCapability(Status.HOSTILE_TO_ENEMY)) {
-                    //check if in or out attack range around
-                    if (distance(map.locationOf(this), exit.getDestination()) == 1) {
-                        Action attackAction = ManFlyAttack(nearbyActor, exit.getDestination().toString());
-                        if(attackAction != null) {
-                            return attackAction;
-                        }
-                    } else {
-                        //if not in attack range, follow
-                        if (this.followBehaviour == null) {
-                            this.followBehaviour = new FollowBehaviour(nearbyActor);
-                        }return followBehaviour.getAction(this, map);
-                    }
-                }
-            }
-        }
-        return wanderBehaviour.getAction(this, map);
-    }
-
-    /**
-     * calculate distance method from followbehaviour class
-     * @param a location a
-     * @param b location b
-     * @return the distance between
-     */
-    private int distance(Location a, Location b) {
-        return Math.abs(a.x() - b.x()) + Math.abs(a.y() - b.y());
-    }
-
-
-    /**
-     * attack action method only for man_fly
-     * 25% chance to hit player, if hit on target, have 30% chance to cause poisoneffect
-     *
-     * @param player as attacking target
-     * @param direction direction to attack
-     * @return action of man_fly attacking
-     */
-    public Action ManFlyAttack(Actor player, String direction) {
-        if(random.nextInt(100)<25){
-            if(random.nextInt(100)<30){
-                player.addStatusEffect(new ManFlyPoisonEffect()); //if poison on add effect on player
-            }
-            return new AttackAction(player,direction); //keep attacking if not poison on
-        }
-        return null; //if attack miss
-    }
 }
---
+++ src/game/weapons/ManFlyStinger.java
@@ -0,0 +1,53 @@
+package game.weapons;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Exit;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
+import game.actions.AttackAction;
+import game.enums.ManFlyPoisonEffect;
+import game.enums.Status;
+
+import java.util.Random;
+
+public class ManFlyStinger extends IntrinsicWeapon {
+    private final static int manFlyDamage = 20;
+    private final static String stingerVerb = "stings";
+    private final static int manFlyHitRate = 25;
+    private final static int poisonStingChance = 30;
+
+
+    private final Random random = new Random();
+
+    /**
+     * constructor
+     */
+    public ManFlyStinger() {
+        super(manFlyDamage, stingerVerb, manFlyHitRate);
+    }
+
+
+    /**
+     * attack action method only for man_fly
+     * 25% chance to hit player, if hit on target, have 30% chance to cause poison effect
+     *
+     * @param attacker the actor who performed the attack
+     * @param target   the actor who is the target of the attack
+     * @param map      the map on which the attack was executed
+     * @return
+     */
+    @Override
+    public String attack(Actor attacker, Actor target, GameMap map) {
+        if (!(random.nextInt(100) < this.hitRate)) {
+            return attacker + " misses " + target + ".";
+        }
+        if(random.nextInt(100) < poisonStingChance){
+            target.addStatusEffect(new ManFlyPoisonEffect()); //if poison on add effect on player
+            return ("actor hits target and poison applied to " + target);
+        }
+        target.hurt(damage);
+
+        return String.format("%s %s %s for %d damage", attacker, verb, target, damage);
+    }
+}
