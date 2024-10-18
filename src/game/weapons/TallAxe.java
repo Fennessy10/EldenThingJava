0aa978681513e1eeb85177ddd8a4f96575df8bfd
pfen0005 <pfen0005@student.monash.edu>
2024-10-18 15:13:07 +1100

    added 10 damage to axe

--- src/game/weapons/TallAxe.java
+++ src/game/weapons/TallAxe.java
@@ -5,7 +5,7 @@ import game.weather.WeatherAffected;
 public class TallAxe extends WeaponItem implements WeatherAffected {
     public TallAxe(WeaponArt weaponArt){
 
-        super("TallAxe",'‡', 60, "attack", 80, 15);
+        super("TallAxe",'‡', 70, "attack", 80, 15);
 
         this.setWeaponArt(weaponArt);
     }
