6892e22f123fcc928b559b43dc88399480804a06
briclare <bvau0003@student.monash.edu>
2024-10-27 22:50:59 +1100

    Modificaiton of Behaviour ArrayList to be accessible by subclasses. i.e. DivineBeast

--- src/game/actors/Enemy.java
+++ src/game/actors/Enemy.java
@@ -12,36 +12,36 @@ import game.behaviours.AttackBehaviour;
 import game.behaviours.WanderBehaviour;
 import game.enums.Status;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 public abstract class Enemy extends Actor {
-    private final Map<Integer, Behaviour> behaviours = new TreeMap<>();
+    protected Map<Integer, Behaviour> behaviours = new HashMap<>();
 
     /**
      * Constructor
-     * Initializes enemy with a name, display character, hit points, and default behaviours.
-     * By default, an enemy has an attack behaviour with high priority
-     * and a wander behaviour with low priority.
-     *
-     * @param name        The name of the enemy
+     * Initializes enemy with a name, display character, hit points and default behaviours.
+     * By default, an enemy has an attack behaviour with high priority and a wander behaviour priority with a low priority.
+     * @param name The name of the enemy
      * @param displayChar The character that represents the enemy on the map
-     * @param hitPoints   The initial hit points (health) of the enemy
+     * @param hitPoints The initial hit points (health) of the enemy
      */
     public Enemy(String name, char displayChar, int hitPoints) {
         super(name, displayChar, hitPoints);
         this.addCapability(Status.ENEMY);
         this.behaviours.put(1, new AttackBehaviour());
         this.behaviours.put(999, new WanderBehaviour());
+
     }
 
     /**
-     * Adds or updates a behaviour for the enemy with a specific priority.
-     * Lower values indicate higher precedence in behaviour execution.
+     * Adds or updates behaviour for enemy with a specific priority
+     * Lower values indicate higher precedence in behaviour execution
      *
-     * @param priority  The priority of the behaviour in the TreeMap
+     * @param priority The priority of the behaviour in the behaviours HashMap
      * @param behaviour The behaviour to add or update in the map of behaviours
      */
+
     public void behavioursPut(int priority, Behaviour behaviour) {
         this.behaviours.put(priority, behaviour);
     }
@@ -50,30 +50,27 @@ public abstract class Enemy extends Actor {
      * Select and return an action to perform on the current turn.
      *
      * @param actions    collection of possible Actions for this Actor
-     * @param lastAction The Action this Actor took last turn.
-     *                   Can do interesting things with Action.getNextAction().
-     * @param map        The map containing the Actor
-     * @param display    The I/O object to which messages may be written
-     * @return The Action to be performed
+     * @param lastAction The Action this Actor took last turn. Can do interesting things in conjunction with Action.getNextAction()
+     * @param map        the map containing the Actor
+     * @param display    the I/O object to which messages may be written
+     * @return the Action to be performed
      */
     @Override
     public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
         for (Behaviour behaviour : behaviours.values()) {
             Action action = behaviour.getAction(this, map);
-            if (action != null) {
+            if (action != null)
                 return action;
-            }
         }
         return new DoNothingAction();
     }
-
     /**
-     * Returns a collection of Actions that the otherActor can do to this enemy.
-     * By default, an actor hostile to the enemy can attack it.
+     * Returns a new collection of the Actions that the otherActor can do to the enemy.
+     * By default, an actor hostile to the enemy, can attack it.
      *
-     * @param otherActor The Actor that might perform the attack
-     * @param direction  The direction of the other Actor
-     * @param map        The current GameMap
+     * @param otherActor the Actor that might be performing attack
+     * @param direction  String representing the direction of the other Actor
+     * @param map        current GameMap
      * @return A collection of Actions.
      */
     @Override
@@ -86,4 +83,3 @@ public abstract class Enemy extends Actor {
         return actions;
     }
 }
-
