893ec4572a28323c8322f5724814834077efedaf
briclare <bvau0003@student.monash.edu>
2024-10-27 21:36:26 +1100

    Creation of Intrinsic Weapon class for the Divine Beast

---
+++ src/game/weapons/SharpTeeth.java
@@ -0,0 +1,9 @@
+package game.weapons;
+
+import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
+
+public class SharpTeeth extends IntrinsicWeapon {
+    public SharpTeeth() {
+        super(150, "bties", 30);
+    }
+}
