232451fa907c55fc562fd1899d90d9817f514df4
lastlyss <sais0004@student.monash.edu>
2024-10-27 20:50:40 +1100

    Implemented Memento weapon art
    - Added the Memento class that extends WeaponArt
    - Integrated health cost logic, with Memento deducting 5 health points per use.
    - Used `List<ActorState>` to store multiple saved states
    - Added logic to save and restore ActorStates whenever the Memento weapon art is consumed

---
+++ src/game/weapons/Memento.java
@@ -0,0 +1,53 @@
+package game.weapons;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.actors.attributes.ActorAttributeOperations;
+import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.actors.ActorState;
+import game.enums.NewActorAttributes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Memento extends WeaponArt {
+
+    private final static int HEALTH_COST = 5;
+    private final List<ActorState> savedStates = new ArrayList<>();
+
+    public Memento() {
+        super(HEALTH_COST);
+    }
+
+    @Override
+    public String activate(Actor player, GameMap map) {
+        if (player.getAttribute(BaseActorAttributes.HEALTH) <= HEALTH_COST) {
+            return String.format("%s's health is too low to use Memento.", player);
+        }
+
+        player.hurt(HEALTH_COST);
+
+        if (Math.random() < 0.5) {
+            saveState(player);
+            return String.format("%s saves their current state using Memento.", player);
+        } else {
+            return restoreState(player);
+        }
+    }
+
+    private void saveState(Actor player) {
+        savedStates.add(new ActorState(player.getAttribute(BaseActorAttributes.HEALTH), player.getAttribute(BaseActorAttributes.MANA) , player.getAttribute(NewActorAttributes.STRENGTH)));
+    }
+
+    private String restoreState(Actor player) {
+        if (!savedStates.isEmpty()) {
+            ActorState lastState = savedStates.remove(savedStates.size() - 1);
+            player.modifyAttribute(BaseActorAttributes.HEALTH,ActorAttributeOperations.UPDATE,lastState.getHealth());
+            player.modifyAttribute(BaseActorAttributes.MANA,ActorAttributeOperations.UPDATE,lastState.getMana());
+            player.modifyAttribute(NewActorAttributes.STRENGTH,ActorAttributeOperations.UPDATE,lastState.getStrength());
+            return String.format("%s restores their last saved state using Memento.", player);
+        } else {
+            return "No previous state to restore.";
+        }
+    }
+}
