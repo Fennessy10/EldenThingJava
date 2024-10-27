e2f9ba2606bef4bb21c856cabfc665570797af00
zhaozhao <zhao0011@student.monash.edu>
2024-10-27 16:03:56 +1100

    complete the method of attack for divine beast head

--- src/game/weapons/DivineBeastHead.java
+++ src/game/weapons/DivineBeastHead.java
@@ -1,5 +1,6 @@
 package game.weapons;
 
+import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
 import game.DivinePower;
@@ -13,7 +14,7 @@ public class DivineBeastHead extends WeaponItem{
      */
     public DivineBeastHead(){
         super("Divine Beast Head",'$',150,"bite",30,0);
-        this.currentPower = new Wind();
+        this.currentPower = new Wind();// set wind as every first power
     }
 
     /**
@@ -32,10 +33,20 @@ public class DivineBeastHead extends WeaponItem{
      * @param attacker the actor who performed the attack
      * @param target   the actor who is the target of the attack
      * @param map      the map on which the attack was executed
-     * @return
+     * @return a String describing the attack result
      */
     @Override
     public String attack(Actor attacker, Actor target, GameMap map){
-        return null;
+        Action divineAttackAction = currentPower.divineAttack(attacker,map,target);//get action from divineattack
+
+        String result;
+        if (divineAttackAction != null){
+            result = divineAttackAction.execute(attacker,map);
+        }else { //if divine attack action is null means nothing happens
+            result = String.format("%s uses %s on %s, but nothing happens.", attacker, currentPower.getClass().getSimpleName(), target);
+        }
+
+        currentPower = currentPower.getNextPower(currentPower);//switch to next divine power
+        return result;
     }
 }
